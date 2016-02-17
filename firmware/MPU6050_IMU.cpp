39bcbbb62431fcc8c0d7e9dd43e1094a00ca969e
Christian Rose <christian.j.rose@gmx.de>
2016-02-17 20:02:23 +0100

    Fixing rate issue

--- firmware/MPU6050_IMU.cpp
+++ firmware/MPU6050_IMU.cpp
@@ -10,25 +10,12 @@
 #include <ros.h>
 #include <ros/time.h>
 #include <geometry_msgs/Quaternion.h>
-#include <std_msgs/UInt32.h>
 
 ros::NodeHandle nh;
 
 geometry_msgs::Quaternion quat_msg;
 ros::Publisher quat_pub("quaternion", &quat_msg);
-/*
-std_msgs::UInt32 success_msg;
-ros::Publisher success_pub("success_packets", &success_msg);
 
-std_msgs::UInt32 no_msg;
-ros::Publisher no_pub("no_packets", &no_msg);
-
-std_msgs::Int8 corrupted_msg;
-ros::Publisher corrupted_pub("corrupted_packets", &corrupted_msg);
-
-std_msgs::Int8 overflow_msg;
-ros::Publisher overflow_pub("overflow_packets", &overflow_msg);
-*/
 int ret;
 void setup() {
     Fastwire::setup(400,0);
@@ -37,10 +24,6 @@ void setup() {
     nh.initNode();
 
     nh.advertise(quat_pub);
-    //nh.advertise(success_pub);
-    //nh.advertise(no_pub);
-    //nh.advertise(corrupted_pub);
-    //nh.advertise(overflow_pub);
 
 }
 
@@ -56,32 +39,19 @@ void loop() {
     ret = mympu_update();
 
     switch (ret) {
-	case 0: c++; break;
-	case 1: np++; return;
-	case 2: err_o++; return;
-	case 3: err_c++; return; 
+    case 0: c++; break;
+    case 1: np++; return;
+    case 2: err_o++; return;
+    case 3: err_c++; return;
     default:
-		return;
+        return;
     }
-/*
-    success_msg.data = c;
-    success_pub.publish(&success_msg);
-    no_msg.data = np;
-    no_pub.publish(&no_msg);
 
-    corrupted_msg.data = err_c;
-    corrupted_pub.publish(&corrupted_msg);
-    overflow_msg.data = err_o;
-    overflow_pub.publish(&overflow_msg);
-    */
-    if (!(c%25)) {
+    quat_msg.w = mympu.quat.w;
+    quat_msg.x = mympu.quat.x;
+    quat_msg.y = mympu.quat.y;
+    quat_msg.z = mympu.quat.z;
 
-        quat_msg.w = mympu.quat.w;
-        quat_msg.x = mympu.quat.x;
-        quat_msg.y = mympu.quat.y;
-        quat_msg.z = mympu.quat.z;
-
-        quat_pub.publish(&quat_msg);
-    }
+    quat_pub.publish(&quat_msg);
 }
 

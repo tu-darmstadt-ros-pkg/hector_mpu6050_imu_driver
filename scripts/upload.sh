a9297b80fb9eebfc1f52c380427fc82c2f7880a8
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2016-02-22 12:38:51 +0100

    Fix package name to be standard compliant

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This only works when in the workspace root, has to be made more robust
-catkin_make hector_MPU6050_imu_driver_firmware_MPU6050_imu-upload
+catkin_make hector_MPU6050_imu_driver_firmware_mpu6050_imu-upload

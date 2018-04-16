62ff7f59815dc0ce73423d3a75bb5bbf435bb303
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2018-04-16 22:14:36 +0200

    Update upload script for use with catkin_tools

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # This only works when in the workspace root, has to be made more robust
-catkin_make hector_mpu6050_imu_driver_firmware_mpu6050_imu-upload
+
+# Obsolete, using plain catkin
+#catkin_make hector_mpu6050_imu_driver_firmware_mpu6050_imu-upload
+
+# For use with catkin tools, see https://github.com/ros-drivers/rosserial/issues/244
+catkin build --no-deps hector_mpu6050_imu_driver --make-args hector_mpu6050_imu_driver_firmware_mpu6050_imu-upload
